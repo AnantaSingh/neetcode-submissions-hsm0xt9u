02b44209c8e9185ca54037f0c344a4ebc45ad472
AnantaSingh <37509541+AnantaSingh@users.noreply.github.com>
2026-03-31 18:15:14 -0700

    Add: minimum-window-with-characters - submission-12

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-12.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.isEmpty() || s.length() < t.length()) {
+            return "";
+        }
+
+        Map<Character, Integer> target = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            target.put(c, target.getOrDefault(c, 0) + 1);
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+        int have = 0;
+        int need = target.size();
+
+        int left = 0;
+        int bestStart = 0;
+        int bestLen = Integer.MAX_VALUE;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (target.containsKey(c) && window.get(c).intValue() == target.get(c).intValue()) {
+                have++;
+            }
+
+            while (have == need) {
+                int windowLen = right - left + 1;
+                if (windowLen < bestLen) {
+                    bestLen = windowLen;
+                    bestStart = left;
+                }
+
+                char remove = s.charAt(left);
+                window.put(remove, window.get(remove) - 1);
+
+                if (target.containsKey(remove) && window.get(remove) < target.get(remove)) {
+                    have--;
+                }
+
+                left++;
+            }
+        }
+
+        return bestLen == Integer.MAX_VALUE ? "" : s.substring(bestStart, bestStart + bestLen);
+    }
+}
