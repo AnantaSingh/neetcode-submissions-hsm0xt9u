14912dfcc22183915703b5f010f75ee958959093
AnantaSingh <37509541+AnantaSingh@users.noreply.github.com>
2026-04-01 12:02:49 -0700

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] cars = new int[n][2]; // [position, speed]
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        // Sort by position descending (closest to target first)
+        Arrays.sort(cars, (a, b) -> Integer.compare(b[0], a[0]));
+
+        // Deque<Double> stack = new ArrayDeque<>(); // holds times to reach target; non-decreasing
+        int fleet = 0;
+        double lastTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            double time = (target - cars[i][0]) / (double) cars[i][1];
+             //System.out.println(cars[i][0] + " " + time);
+
+            // If current time <= top time, it catches up → same fleet → keep the slower (top).
+            if (time > lastTime) {
+                // new fleet (can’t catch up to the one ahead)
+                lastTime = time;
+                fleet++; 
+            }
+        }
+
+        return fleet;
+    }
+}
