ec7e44cbfd6dd15b13fbd4d82b235b6ae1af79fc
AnantaSingh <37509541+AnantaSingh@users.noreply.github.com>
2026-04-01 12:24:11 -0700

    Add: largest-rectangle-in-histogram - submission-5

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-5.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Deque<int[]> stack = new ArrayDeque<>();// each element = [index, height]
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            int start = i;
+
+            // while current bar is shorter than the one on stack top
+            // pop from stack and calculate area
+            while (!stack.isEmpty() && stack.peek()[1] > heights[i]) {
+                int[] top = stack.pop();
+                int index = top[0];
+                int height = top[1];
+                maxArea = Math.max(maxArea, height * (i - index));
+                start = index; // we can extend the new bar to this popped index
+            }
+
+            stack.push(new int[]{start, heights[i]});
+        }
+
+        // handle bars still in stack
+        while (!stack.isEmpty())
+        {
+            int[] pair = stack.pop();
+            int index = pair[0];
+            int height = pair[1];
+            maxArea = Math.max(maxArea, height * (heights.length - index));
+        }
+
+        return maxArea;
+    }
+}
