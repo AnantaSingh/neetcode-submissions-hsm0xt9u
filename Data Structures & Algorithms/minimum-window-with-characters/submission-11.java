40716af8e754fb35252bb2054ef79b47ffdf4cdc
AnantaSingh <37509541+AnantaSingh@users.noreply.github.com>
2026-03-31 18:09:42 -0700

    Add: minimum-window-with-characters - submission-11

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-11.java
@@ -0,0 +1,70 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        if(t.isEmpty())
+            return "";
+
+        //2 HM HAVE NEED
+
+        // maintain 2 hashmap
+        Map<Character, Integer> tcount = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+
+        for(int i=0; i < t.length(); i++)
+        {
+            // this hashMap will not change  
+            tcount.put(t.charAt(i), tcount.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        // have 2 counters for have and need
+        int have = 0;
+        int need = tcount.size();
+
+        // to store res
+        int[] res = {-1, -1}; // NEW WAY TO STORE
+        int resLen = Integer.MAX_VALUE; // ALWAYS FORGET
+
+        // for 2 pointers l and r
+        int l = 0;
+
+        for(int r = 0; r < s.length(); r++)
+        {
+            // add the char to hashmap
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            // now a char has come so need to increase have if needed
+            // increaing have
+            if(tcount.containsKey(c) && window.get(c) == tcount.get(c))
+            {
+                have++;
+            }
+
+            while(have == need)
+            {
+                // see if we need to update answer
+                if((r - l + 1) < resLen)
+                {
+                    res[0] = l;
+                    res[1] = r;
+                    resLen = r - l + 1;
+                }
+
+                //now removing the left char so first store it
+                char leftchar = s.charAt(l);
+                window.put(leftchar, window.get(leftchar) - 1);
+                // decreasing have similar to above increasing
+                if(tcount.containsKey(leftchar) && window.get(leftchar) < tcount.get(leftchar))
+                {
+                    have--;
+                }
+                l++;
+            }
+        }
+
+
+
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1]+1);
+        
+    }
+}
